c0244f8d956523239f1918c02c5f6b48e41fdfd0
Ayman <aymanbourramouss@gmail.com>
2023-06-02 18:49:31 +0200

    imaging

--- executors/lithops_executor.py
+++ executors/lithops_executor.py
@@ -7,8 +7,13 @@ class LithopsExecutor(Executor):
         self.executor = lithops.FunctionExecutor()
 
     def execute(self, step, iterdata: List[str], extra_args: List[str], extra_env: dict):
-    
-
         futures = self.executor.map(step.run, iterdata, extra_args=extra_args, extra_env=extra_env)
         results = self.executor.get_result(futures)
         return results
+    
+    def execute_call_async(self, step, iterdata: List[str], extra_args: List[str], extra_env: dict):
+        futures = self.executor.call_async(step.run, (iterdata, *extra_args), extra_env=extra_env)
+        results = self.executor.get_result(futures)
+        return results
+    
+    
--- steps/imaging.py
+++ steps/imaging.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 from .step import Step
-
+from datasource import LithopsDataSource
 class ImagingStep(Step):
     
     def __init__(self, output_name):
@@ -9,10 +9,12 @@ class ImagingStep(Step):
 
     def run(self, input_files: list, bucket_name: str, output_dir: str):
         
+        self.datasource = LithopsDataSource()
         
-        #self.datasource.download(bucket_name, calibrated_mesurement_set, output_dir)
+        os.chdir(output_dir)
+        for calibrated_mesurement_set in input_files:
+            self.datasource.download(bucket_name, calibrated_mesurement_set, output_dir)
 
-        
         cmd = [
                 "wsclean",
                 "-size", "1024", "1024",
@@ -36,7 +38,11 @@ class ImagingStep(Step):
         # Append all the input files to the command
         cmd.extend(input_files)
         
+        print(cmd)
         out = subprocess.run(cmd, capture_output=True, text=True)
         
         print(out.stdout)
         print(out.stderr)
+        
+        print(f'Uploading {output_dir}/{self.output_name}.fits')
+        self.datasource.storage.upload_file(f'extract-data/step3_out/{self.output_name}-image.fits', bucket_name ,f'{output_dir}/{self.output_name}.fits')
