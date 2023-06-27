c59c51b1c4078156dbec27acc4d25e53be327329
Ayman <aymanbourramouss@gmail.com>
2023-06-27 16:00:19 +0200

    timers to seconds

--- utils/decorators.py
+++ utils/decorators.py
@@ -40,8 +40,8 @@ def timeit_execution(method):
         te = time.time()
         if 'log_time' in kw:
             name = kw.get('log_name', method.__name__.upper())
-            kw['log_time'][name] = int((te - ts) * 1000)
+            kw['log_time'][name] = te - ts
         else:
-            print(f"{method.__name__}  {(te - ts) * 1000} ms")
-        return (te - ts) * 1000  # Return only elapsed time in milliseconds
+            print(f"{method.__name__}  {(te - ts)} seconds")
+        return te - ts
     return timed
