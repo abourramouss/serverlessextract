76e62ddb9a785c679d7567d638cd4a3aaa7a23f0
Ayman <aymanbourramouss@gmail.com>
2023-07-12 13:03:17 +0200

    move param files as dict
    It enables dynamic writting of the params.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,9 @@
-from .utils import delete_all_in_cwd
+from .utils import (
+    delete_all_in_cwd,
+    S3Path,
+    rebinning_param_parset,
+    cal_param_parset,
+    sub_param_parset,
+    apply_cal_param_parset,
+)
 from .decorators import timeit_io, timeit_execution
