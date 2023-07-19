d52e8492847af9433315aa4a3324b36f994cf478
Ayman <aymanbourramouss@gmail.com>
2023-07-19 17:05:15 +0200

    lithops executor wip

--- local_pipeline.py
+++ local_pipeline.py
@@ -11,7 +11,13 @@ import shutil
 import lithops
 from lithops import Storage
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from util.helpers import S3Path, rebinning_param_parset, cal_param_parset, sub_param_parset, apply_cal_param_parset
+from util.helpers import (
+    S3Path,
+    rebinning_param_parset,
+    cal_param_parset,
+    sub_param_parset,
+    apply_cal_param_parset,
+)
 from pathlib import PurePosixPath
 import multiprocessing
 import os
@@ -19,164 +25,6 @@ import shutil
 from pathlib import PurePath, _PosixFlavour
 import logging
 from contextlib import suppress
-
-rebinning_param_parset = {
-    "msin": "",
-    "msout": "",
-    "steps": "[aoflag, avg, count]",
-    "aoflag.type": "aoflagger",
-    "aoflag.memoryperc": 90,
-    "avg.type": "averager",
-    "avg.freqstep": 4,
-    "avg.timestep": 8,
-}
-
-cal_param_parset = {
-    "msin": "",
-    "msin.datacolumn": "DATA",
-    "msout": ".",
-    "steps": "[cal]",
-    "cal.type": "ddecal",
-    "cal.mode": "diagonal",
-    "cal.h5parm": "",
-    "cal.solint": 4,
-    "cal.nchan": 4,
-    "cal.maxiter": 50,
-    "cal.uvlambdamin": 5,
-    "cal.smoothnessconstraint": 2e6,
-}
-
-sub_param_parset = {
-    "msin": "",
-    "msin.datacolumn": "DATA",
-    "msout": ".",
-    "msout.datacolumn": "SUBTRACTED_DATA",
-    "steps": "[sub]",
-    "sub.type": "h5parmpredict",
-    "sub.directions": "[[CygA], [CasA]]",
-    "sub.operation": "subtract",
-    "sub.applycal.parmdb": "",
-    "sub.applycal.steps": "[sub_apply_amp, sub_apply_phase]",
-    "sub.applycal.correction": "fulljones",
-    "sub.applycal.sub_apply_amp.correction": "amplitude000",
-    "sub.applycal.sub_apply_phase.correction": "phase000",
-}
-
-apply_cal_param_parset = {
-    "msin": "",
-    "msin.datacolumn": "SUBTRACTED_DATA",
-    "msout": ".",
-    "msout.datacolumn": "CORRECTED_DATA",
-    "steps": "[apply]",
-    "apply.type": "applycal",
-    "apply.steps": "[apply_amp, apply_phase]",
-    "apply.apply_amp.correction": "amplitude000",
-    "apply.apply_phase.correction": "phase000",
-    "apply.direction": "[Main]",
-    "apply.parmdb": "",
-}
-class _S3Flavour(_PosixFlavour):
-    is_supported = True
-
-    def parse_parts(self, parts):
-        drv, root, parsed = super().parse_parts(parts)
-        for part in parsed[1:]:
-            if part == "..":
-                index = parsed.index(part)
-                parsed.pop(index - 1)
-                parsed.remove(part)
-        return drv, root, parsed
-
-    def make_uri(self, path):
-        uri = super().make_uri(path)
-        return uri.replace("file:///", "s3://")
-
-
-class S3Path(PurePath):
-    """
-    PurePath subclass for AWS S3 service
-    Source: https://github.com/liormizr/s3path
-    S3 is not a file-system, but we can look at it like a POSIX system
-    """
-
-    _flavour = _S3Flavour()
-    __slots__ = ()
-
-    @classmethod
-    def from_uri(cls, uri: str) -> "S3Path":
-        """
-        from_uri class method create a class instance from url
-
-        >> from s3path import PureS3Path
-        >> PureS3Path.from_url('s3://<bucket>/<key>')
-        << PureS3Path('/<bucket>/<key>')
-        """
-        if not uri.startswith("s3://"):
-            raise ValueError("Provided uri seems to be no S3 URI!")
-        return cls(uri[4:])
-
-    @classmethod
-    def from_bucket_key(cls, bucket: str, key: str) -> "S3Path":
-        """
-        from_bucket_key class method create a class instance from bucket, key pair's
-
-        >> from s3path import PureS3Path
-        >> PureS3Path.from_bucket_key(bucket='<bucket>', key='<key>')
-        << PureS3Path('/<bucket>/<key>')
-        """
-        bucket = cls(cls._flavour.sep, bucket)
-        if len(bucket.parts) != 2:
-            raise ValueError(
-                "bucket argument contains more then one path element: {}".format(bucket)
-            )
-        key = cls(key)
-        if key.is_absolute():
-            key = key.relative_to("/")
-        return bucket / key
-
-    @property
-    def bucket(self) -> str:
-        """
-        The AWS S3 Bucket name, or ''
-        """
-        self._absolute_path_validation()
-        with suppress(ValueError):
-            _, bucket, *_ = self.parts
-            return bucket
-        return ""
-
-    @property
-    def key(self) -> str:
-        """
-        The AWS S3 Key name, or ''
-        """
-        self._absolute_path_validation()
-        key = self._flavour.sep.join(self.parts[2:])
-        return key
-
-    @property
-    def virtual_directory(self) -> str:
-        """
-        The parent virtual directory of a key
-        Example: foo/bar/baz -> foo/baz
-        """
-        vdir, _ = self.key.rsplit("/", 1)
-        return vdir
-
-    def as_uri(self) -> str:
-        """
-        Return the path as a 's3' URI.
-        """
-        return super().as_uri()
-
-    def _absolute_path_validation(self):
-        if not self.is_absolute():
-            raise ValueError("relative path have no bucket, key specification")
-
-    def __repr__(self) -> str:
-        return "{}(bucket={},key={})".format(
-            self.__class__.__name__, self.bucket, self.key
-        )
 
 
 class PipelineStep(ABC):
@@ -207,12 +55,15 @@ class LithopsExecutor(Executor):
     def __init__(self):
         self._executor = lithops.FunctionExecutor()
 
-    #Call execute as a single call async function
+    # Call execute as a single call async function
     def execute(self, runner: Callable, parameters) -> None:
         futures = self._executor.call_async(runner, parameters)
         self._executor.get_result(fs=futures)
 
-    
+    def execute_map(self, runner: Callable, parameters) -> None:
+        futures = self._executor.map(runner, parameters)
+        self._executor.get_result(fs=futures)
+
 
 # Four operations: download file, download directory, upload file, upload directory (Multipart) to interact with pipeline files
 class DataSource(ABC):
@@ -240,6 +91,7 @@ class DataSource(ABC):
     ) -> None:
         pass
 
+
 def remove_cached(parameters):
     for param in parameters:
         if "parameter_file_path" in parameters[param]:
@@ -252,9 +104,7 @@ def remove_cached(parameters):
     if os.path.exists(parameters[CalibrationStep.__name__]["output_h5"]):
         os.remove(parameters[CalibrationStep.__name__]["output_h5"])
 
-    if os.path.exists(
-        os.path.dirname(parameters[ImagingStep.__name__]["output_dir"])
-    ):
+    if os.path.exists(os.path.dirname(parameters[ImagingStep.__name__]["output_dir"])):
         images = os.listdir(
             os.path.dirname(parameters[ImagingStep.__name__]["output_dir"])
         )
@@ -266,13 +116,13 @@ def remove_cached(parameters):
                 )
             )
 
+
 def write_parset_dict_to_file(parset_dict: dict, filename: str):
-    
     filename = f"/tmp{filename}"
     print(f"Writing parset file to {filename}")
     if not os.path.exists(os.path.dirname(filename)):
         os.makedirs(os.path.dirname(filename), exist_ok=True)
-    
+
     with open(filename, "w") as f:
         for key, value in parset_dict.items():
             f.write(f"{key}={value}\n")
@@ -282,22 +132,34 @@ class LocalDataSource(DataSource):
     def download_file(
         self, read_path: Union[S3Path, PurePosixPath], write_path: PurePosixPath
     ) -> None:
-        pass
+        if read_path == write_path:
+            print("Same path, skipping download")
+        else:
+            shutil.copy(read_path, write_path)
 
     def download_directory(
         self, read_path: Union[S3Path, PurePosixPath], write_path: PurePosixPath
     ) -> None:
-        pass
+        if read_path == write_path:
+            print("Same path, skipping download")
+        else:
+            shutil.copytree(read_path, write_path)
 
     def upload_file(
         self, read_path: PurePosixPath, write_path: Union[S3Path, PurePosixPath]
     ) -> None:
-        pass
+        if read_path == write_path:
+            print("Same path, skipping download")
+        else:
+            shutil.copy(read_path, write_path)
 
     def upload_directory(
         self, read_path: PurePosixPath, write_path: Union[S3Path, PurePosixPath]
     ) -> None:
-        pass
+        if read_path == write_path:
+            print("Same path, skipping download")
+        else:
+            shutil.copytree(read_path, write_path)
 
 
 class LithopsDataSource(DataSource):
@@ -428,15 +290,8 @@ class Pipeline:
 
         self._executor = executor
         self._datasource = datasource
-    @staticmethod
-    def download_ms(parameters):
-        Pipeline._datasource.download_directory(
-            parameters[RebinningStep.__name__]["measurement_set"],
-            parameters[RebinningStep.__name__]["write_path"],
-        )
-    
-    @staticmethod
-    def prepare_parameters(parameters):
+
+    def prepare_parameters(self, parameters):
         # Create/modify parset files in the worker and download the lua and source db files.
         write_parset_dict_to_file(
             rebinning_param_parset,
@@ -455,24 +310,51 @@ class Pipeline:
             parameters[ApplyCalibrationStep.__name__]["parameter_file_path"],
         )
 
-        
+        # Download ms
+        self._datasource.download_directory(
+            parameters[RebinningStep.__name__]["measurement_set"],
+            parameters[RebinningStep.__name__]["write_path"],
+        )
+        # Download the lua and source db files
+        self._datasource.download_file(
+            parameters[RebinningStep.__name__]["lua_file_dir"]["origin"],
+            parameters[RebinningStep.__name__]["lua_file_dir"]["destination"],
+        )
+        self._datasource.download_directory(
+            parameters[CalibrationStep.__name__]["sourcedb_directory"]["origin"],
+            parameters[CalibrationStep.__name__]["sourcedb_directory"]["destination"],
+        )
+
     @staticmethod
     def execute_steps(steps) -> None:
         for step in steps:
             step(parameters[step.__class__.__name__])
-    
-    
+
     @staticmethod
     def run_pipeline(parameters):
         Pipeline.prepare_parameters(parameters)
-        
-        
 
-    # Depending on the executor and datasource, we should prepare the parameters for execution i.e download ms and parameter files
+    @classmethod
+    def create_and_run(cls, parameters, executor_type, datasource_type):
+        if executor_type == "local":
+            executor = LocalExecutor()
+        elif executor_type == "lithops":
+            executor = LithopsExecutor()
+        else:
+            raise ValueError(f"Unknown executor type: {executor_type}")
+
+        if datasource_type == "local":
+            datasource = LocalDataSource()
+        elif datasource_type == "lithops":
+            datasource = LithopsDataSource()
+        else:
+            raise ValueError(f"Unknown datasource type: {datasource_type}")
+
+        pipeline = cls(parameters, executor, datasource)
+        pipeline.run()
 
     def run(self):
-        self._executor.execute(Pipeline.run_pipeline
-                               , self.parameters)
+        self._executor.execute(Pipeline.run_pipeline, self.parameters)
         # self.run_imaging()
 
 
@@ -483,7 +365,11 @@ class Pipeline:
 #   - write_path: creates a new measurement set in the write path
 class RebinningStep(PipelineStep):
     def build_command(
-        self, measurement_set: str, parameter_file_path: str, lua_file_dir: str, write_path: str
+        self,
+        measurement_set: str,
+        parameter_file_path: str,
+        lua_file_dir: str,
+        write_path: str,
     ) -> List[str]:
         return [
             "DP3",
@@ -657,7 +543,14 @@ if __name__ == "__main__":
                 "parameter_file_path": PurePosixPath(
                     f"{parameters_write_path}/rebinning/STEP1-flagrebin.parset"
                 ),
-                "lua_file_dir": PurePosixPath("/home/ayman/Downloads/pipeline/parameters/rebinning/STEP1-NenuFAR64C1S.lua"),
+                "lua_file_dir": {
+                    "origin": PurePosixPath(
+                        "/home/ayman/Downloads/pipeline/parameters/rebinning/STEP1-NenuFAR64C1S.lua"
+                    ),
+                    "destination": PurePosixPath(
+                        "/home/ayman/Downloads/pipeline/parameters/rebinning/STEP1-NenuFAR64C1S.lua"
+                    ),
+                },
                 "write_path": PurePosixPath(calibrated_ms),
             },
             "CalibrationStep": {
@@ -666,7 +559,10 @@ if __name__ == "__main__":
                     f"{parameters_write_path}/cal/STEP2A-calibration.parset"
                 ),
                 "output_h5": PurePosixPath(h5),
-                "sourcedb_directory": sourcedb_directory,
+                "sourcedb_directory": {
+                    "origin": sourcedb_directory,
+                    "destination": sourcedb_directory,
+                },
             },
             "SubstractionStep": {
                 "calibrated_measurement_set": PurePosixPath(calibrated_ms),
@@ -674,7 +570,10 @@ if __name__ == "__main__":
                     f"{parameters_write_path}/sub/STEP2B-subtract.parset"
                 ),
                 "input_h5": PurePosixPath(h5),
-                "sourcedb_directory": sourcedb_directory,
+                "sourcedb_directory": {
+                    "origin": sourcedb_directory,
+                    "destination": sourcedb_directory,
+                },
             },
             "ApplyCalibrationStep": {
                 "calibrated_measurement_set": calibrated_ms,
@@ -691,7 +590,7 @@ if __name__ == "__main__":
     # Run pipeline with parameters
     pipeline = Pipeline(
         parameters=parameters,
-        executor=LithopsExecutor(),
-        datasource=LithopsDataSource(),
+        executor=LocalExecutor(),
+        datasource=LocalDataSource(),
     )
     pipeline.run()
