0a018b577b249c96f5d7f1b01f4d3db8adf6e739
Ayman <aymanbourramouss@gmail.com>
2023-11-22 18:31:46 +0100

    more plots

--- util/profiler.py
+++ util/profiler.py
@@ -63,6 +63,7 @@ class Profiler:
             f"Disk Write Rate (MB/s): {self.disk_write}\n"
             f"Bytes sent (MB/s): {self.net_write}\n"
             f"Bytes received (MB/s): {self.net_read}\n"
+            f"Timerecords: {self.time_records}\n"
             f"Timestamps: {self.timestamps}\n"
         )
 
