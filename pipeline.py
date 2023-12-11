9ad68e971cb58688af9ff0934068cbcdb34b95ba
Ayman <aymanbourramouss@gmail.com>
2023-12-11 19:20:19 +0100

    profiler collection stats

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,8 @@ from steps.imaging import imaging, monitor_and_run_imaging
 from s3path import S3Path
 from util import ProfilerCollection
 from lithops import Storage
-
+from util import CPUMetric, MemoryMetric, DiskMetric, NetworkMetric
+import os
 
 parameters = {
     "RebinningStep": {
@@ -140,3 +141,118 @@ for step_profiler in collection:
         for metric in profiler:
             print(metric)
         print("-------------------")
+
+
+import matplotlib.pyplot as plt
+from dataclasses import fields
+import os
+
+
+def aggregate_and_plot(collection, save_dir, filename):
+    # Initialize dictionaries for aggregated metrics and timestamps
+    aggregated_metrics = {}
+    timestamps = {}
+
+    # Collect and sum metrics by collection_id
+    for step_profiler in collection:
+        for profiler in step_profiler:
+            for metric in profiler:
+                cid = metric.collection_id
+
+                # Sum the metrics for each collection_id
+                if cid not in aggregated_metrics:
+                    aggregated_metrics[cid] = {
+                        "cpu_usage": 0,
+                        "memory_usage": 0,
+                        "disk_read_mb": 0,
+                        "disk_write_mb": 0,
+                        "net_read_mb": 0,
+                        "net_write_mb": 0,
+                    }
+                    timestamps[cid] = []
+
+                timestamps[cid].append(metric.timestamp)
+                aggregated_metrics[cid]["cpu_usage"] += getattr(metric, "cpu_usage", 0)
+                aggregated_metrics[cid]["memory_usage"] += getattr(
+                    metric, "memory_usage", 0
+                )
+                aggregated_metrics[cid]["disk_read_mb"] += getattr(
+                    metric, "disk_read_mb", 0
+                )
+                aggregated_metrics[cid]["disk_write_mb"] += getattr(
+                    metric, "disk_write_mb", 0
+                )
+                aggregated_metrics[cid]["net_read_mb"] += getattr(
+                    metric, "net_read_mb", 0
+                )
+                aggregated_metrics[cid]["net_write_mb"] += getattr(
+                    metric, "net_write_mb", 0
+                )
+
+    # Convert timestamps to relative durations
+    min_timestamp = min(min(ts_list) for ts_list in timestamps.values())
+    duration = {
+        cid: min(ts_list) - min_timestamp for cid, ts_list in timestamps.items()
+    }
+
+    # Prepare data for plotting
+    sorted_cids = sorted(duration, key=duration.get)
+    cpu_usages = [aggregated_metrics[cid]["cpu_usage"] for cid in sorted_cids]
+    memory_usages = [aggregated_metrics[cid]["memory_usage"] for cid in sorted_cids]
+    disk_read_mbs = [aggregated_metrics[cid]["disk_read_mb"] for cid in sorted_cids]
+    disk_write_mbs = [aggregated_metrics[cid]["disk_write_mb"] for cid in sorted_cids]
+    net_read_mbs = [aggregated_metrics[cid]["net_read_mb"] for cid in sorted_cids]
+    net_write_mbs = [aggregated_metrics[cid]["net_write_mb"] for cid in sorted_cids]
+
+    # Plotting
+    plt.figure(figsize=(15, 10))
+    plt.suptitle("Aggregated Profiler Metrics Over Relative Duration", fontsize=20)
+
+    # Create subplots for each metric type
+    plt.subplot(3, 2, 1)
+    plt.plot(sorted_cids, cpu_usages, marker="o")
+    plt.title("CPU Usage")
+    plt.xlabel("Relative Duration (seconds)")
+    plt.ylabel("Total CPU Usage (%)")
+
+    plt.subplot(3, 2, 2)
+    plt.plot(sorted_cids, memory_usages, marker="o")
+    plt.title("Memory Usage")
+    plt.xlabel("Relative Duration (seconds)")
+    plt.ylabel("Total Memory Usage (MB)")
+
+    plt.subplot(3, 2, 3)
+    plt.plot(sorted_cids, disk_read_mbs, marker="o")
+    plt.title("Disk Read")
+    plt.xlabel("Relative Duration (seconds)")
+    plt.ylabel("Total Disk Read (MB)")
+
+    plt.subplot(3, 2, 4)
+    plt.plot(sorted_cids, disk_write_mbs, marker="o")
+    plt.title("Disk Write")
+    plt.xlabel("Relative Duration (seconds)")
+    plt.ylabel("Total Disk Write (MB)")
+
+    plt.subplot(3, 2, 5)
+    plt.plot(sorted_cids, net_read_mbs, marker="o")
+    plt.title("Network Read")
+    plt.xlabel("Relative Duration (seconds)")
+    plt.ylabel("Total Network Read (MB)")
+
+    plt.subplot(3, 2, 6)
+    plt.plot(sorted_cids, net_write_mbs, marker="o")
+    plt.title("Network Write")
+    plt.xlabel("Relative Duration (seconds)")
+    plt.ylabel("Total Network Write (MB)")
+
+    plt.tight_layout()
+
+    # Ensure the save directory exists
+    os.makedirs(save_dir, exist_ok=True)
+
+    # Save the plot
+    plt.savefig(os.path.join(save_dir, filename))
+    plt.close()
+
+
+aggregate_and_plot(collection, "plots", "rebinning.png")
