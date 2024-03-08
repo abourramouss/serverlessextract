9151a7b522ba93269395828777cd1120464c3fe0
Ayman <aymanbourramouss@gmail.com>
2024-03-08 10:44:37 +0100

    aws lambda/ec2

--- plot/__init__.py
+++ plot/__init__.py
@@ -7,4 +7,7 @@ from .plot_functions import (
     plot_cost_vs_time_pareto_real,
     plot_speedup_vs_cost_from_collection,
     plot_memory_speedup_from_collection,
+    plot_cost_vs_time_pareto_real_partition,
+    plot_cost_vs_time_pareto_real_ec2,
+    plot_avg_execution_time_per_instance,
 )
