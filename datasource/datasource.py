c2aad06bf7782f75bd5bf1fd2e7577aa002d7122
Ayman Bourramouss <aymanbourramouss@gmail.com>
2024-04-15 16:43:32 +0000

    modifications on OutputS3 and output data

--- datasource/datasource.py
+++ datasource/datasource.py
@@ -4,7 +4,7 @@ from typing import Union
 from s3path import S3Path
 import zipfile
 import os
-import subprocess
+import shutil
 
 
 class InputS3:
@@ -33,10 +33,11 @@ class InputS3:
 
 
 class OutputS3:
-    def __init__(self, bucket: str, key: str, naming_pattern="{id}.output"):
+    def __init__(self, bucket: str, key: str, file_ext=None, separate_dir=False):
         self._bucket = bucket
         self._key = key
-        self._naming_pattern = naming_pattern
+        self._file_ext = file_ext
+        self._separate_dir = separate_dir
 
     @property
     def bucket(self):
@@ -55,25 +56,15 @@ class OutputS3:
         self._key = value
 
     @property
-    def naming_pattern(self):
-        return self._naming_pattern
+    def file_ext(self):
+        return self._file_ext
 
-    @naming_pattern.setter
-    def naming_pattern(self, value):
-        self._naming_pattern = value
-
-    def formatted_filename(self, **kwargs):
-        return self._naming_pattern.format(**kwargs)
-
-    def construct_s3_path(self, id=None):
-        if id is not None:
-            filename = self.naming_pattern.format(id=id)
-        else:
-            filename = self.naming_pattern.format(id="default")  # default or some logic
-        return f"{self._bucket}/{os.path.join(self._key, filename)}"
+    @property
+    def separate_dir(self):
+        return self._separate_dir
 
     def __str__(self):
-        return f"s3://{self._bucket}/{self._key}/{self.naming_pattern}"
+        return f"/{self._bucket}/{self._key}"
 
 
 # Four operations: download file, download directory, upload file, upload directory (Multipart) to interact with pipeline files
@@ -101,31 +92,34 @@ class DataSource(ABC):
 
     def zip_without_compression(self, ms: PosixPath) -> PosixPath:
         print(f"Zipping directory: {ms}")
-        zip_filepath = PosixPath(f"{ms.parent}/{ms.name}.zip")
-        with zipfile.ZipFile(zip_filepath, "w", zipfile.ZIP_STORED) as zip_file:
+        zip_filepath = ms.with_suffix(".zip")
+
+        if zip_filepath.exists() and zip_filepath.is_dir():
+            raise IsADirectoryError(
+                f"Cannot create a zip file as a directory with the name {zip_filepath} exists."
+            )
+
+        with zipfile.ZipFile(zip_filepath, "w", zipfile.ZIP_STORED) as zipf:
             for root, dirs, files in os.walk(ms):
                 for file in files:
                     file_path = os.path.join(root, file)
                     arcname = os.path.relpath(file_path, start=ms.parent)
-                    zip_file.write(file_path, arcname)
+                    zipf.write(file_path, arcname=arcname)
+
+        print(f"Created zip file at {zip_filepath}")
         return zip_filepath
 
     def unzip(self, ms: PosixPath) -> PosixPath:
-        with zipfile.ZipFile(ms, "r") as zip_file:
-            extract_path = ms.parent
-            zip_file.extractall(extract_path)
+        print(f"Extracting zip file at {ms}")
+        if ms.suffix != ".zip":
+            raise ValueError(f"Expected a .zip file, got {ms}")
 
-            # Collect the first level of directories/files in the zip
-            root_items = {item.split("/")[0] for item in zip_file.namelist()}
+        extract_path = ms.parent / ms.stem
+        if not extract_path.exists():
+            extract_path.mkdir(parents=True, exist_ok=True)
 
-        # Determine if all contents are under a single root directory
-        if len(root_items) == 1:
-            part_name = next(iter(root_items))
-            new_ms_path = extract_path / part_name
-            if not (new_ms_path.exists() and new_ms_path.is_dir()):
-                os.makedirs(new_ms_path, exist_ok=True)
-        else:
-            new_ms_path = extract_path
+        with zipfile.ZipFile(ms, "r") as zipf:
+            zipf.extractall(extract_path)
 
-        print(f"Unzipped to: {new_ms_path}")
-        return new_ms_path
+        print(f"Extracted to: {extract_path}")
+        return extract_path
--- pipeline.py
+++ pipeline.py
@@ -18,70 +18,81 @@ logging.basicConfig(level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:
 logger = logging.getLogger(__name__)
 
 
-start_time = time.time()
-
 rebinning_params = {
-    "flagrebin": {
-        "msin": InputS3(
-            bucket="ayman-extract",
-            key="partitions/partitions_7900_20zip",
-        ),
-        "steps": "[aoflag, avg, count]",
-        "aoflag.type": "aoflagger",
-        "aoflag.strategy": InputS3(
-            bucket="ayman-extract",
-            key="parameters/rebinning/STEP1-NenuFAR64C1S.lua",
-        ),
-        "avg.type": "averager",
-        "avg.freqstep": 4,
-        "avg.timestep": 8,
-        "msout": OutputS3(
-            bucket="ayman-extract",
-            key="extract-data/rebinning_out",
-            naming_pattern="partition_{id}.ms",
-        ),
-    }
+    "msin": InputS3(
+        bucket="ayman-extract",
+        key="partitions/partitions_7900_20zip_1/",
+    ),
+    "steps": "[aoflag, avg, count]",
+    "aoflag.type": "aoflagger",
+    "aoflag.strategy": InputS3(
+        bucket="ayman-extract",
+        key="parameters/rebinning/STEP1-NenuFAR64C1S.lua",
+    ),
+    "avg.type": "averager",
+    "avg.freqstep": 4,
+    "avg.timestep": 8,
+    "msout": OutputS3(
+        bucket="ayman-extract",
+        key="extract-data/rebinning_out",
+        file_ext=".ms",
+    ),
 }
-finished_job = DP3Step(parameters=rebinning_params["flagrebin"]).run(func_limit=1)
 
-
+start_time = time.time()
+finished_job = DP3Step(parameters=rebinning_params).run(func_limit=1)
 end_time = time.time()
+
+"""
 
 
 logger.info(f"Rebinning completed in {end_time - start_time} seconds.")
 
 
 calibration_params = {
-    "cal": {
-        "msin": InputS3(
-            bucket="ayman-extract",
-            key="extract-data/rebinning_out",
-        ),
-        "msin.datacolumn": "DATA",
-        "msout": ".",
-        "steps": "[cal]",
-        "cal.type": "ddecal",
-        "cal.mode": "diagonal",
-        "cal.sourcedb": InputS3(
-            bucket="ayman-extract",
-            key="parameters/calibration/STEP2A-apparent.sourcedb",
-        ),
-        "cal.h5parm": OutputS3(
-            bucket="ayman-extract",
-            key="extract-data/calibration_out",
-            naming_pattern="calibration_{id}.h5.zip",
-        ),
-        "numthreads": 4,
-        "cal.solint": 4,
-        "cal.nchan": 4,
-        "cal.maxiter": 50,
-        "cal.uvlambdamin": 5,
-        "cal.smoothnessconstraint": 2e6,
-    }
+    "msin": InputS3(
+        bucket="ayman-extract",
+        key="extract-data/rebinning_out",
+    ),
+    "msin.datacolumn": "DATA",
+    "msout": ".",
+    "steps": "[cal, sub]",
+    "cal.type": "ddecal",
+    "cal.mode": "diagonal",
+    "cal.sourcedb": InputS3(
+        bucket="ayman-extract",
+        key="parameters/calibration/STEP2A-apparent.sourcedb",
+    ),
+    "cal.h5parm": OutputS3(
+        bucket="ayman-extract",
+        key="extract-data/calibration_out",
+        naming_pattern="calibration_{id}.h5",
+    ),
+    "numthreads": 4,
+    "cal.solint": 4,
+    "cal.nchan": 4,
+    "cal.maxiter": 50,
+    "cal.uvlambdamin": 5,
+    "cal.smoothnessconstraint": 2e6,
+    "msout.datacolumn": "SUBTRACTED_DATA",
+    "steps": "[sub]",
+    "sub.type": "h5parmpredict",
+    "sub.sourcedb": InputS3(
+        bucket="ayman-extract", key="parameters/calibration/STEP2A-apparent.sourcedb"
+    ),
+    "sub.directions": "[[CygA],[CasA]]",
+    "sub.operation": "subtract",
+    "sub.applycal.parmdb": "",
+    "sub.applycal.steps": "[sub_apply_amp,sub_apply_phase]",
+    "sub.applycal.correction": "fulljones",
+    "sub.applycal.sub_apply_amp.correction": "amplitude000",
+    "sub.applycal.sub_apply_phase.correction": "phase000",
 }
 
+
 start_time = time.time()
-# finished_job = DP3Step(parameters=calibration_params["cal"]).run(func_limit=1)
+finished_job = DP3Step(parameters=calibration_params).run(func_limit=1)
 end_time = time.time()
 
 logger.info(f"Calibration completed in {end_time - start_time} seconds.")
+"""
