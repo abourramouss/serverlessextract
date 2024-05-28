291a52ce68565b610a711d3d611165c46ee738e6
Ayman <aymanbourramouss@gmail.com>
2024-05-28 11:40:31 +0200

    readme modifications and add ovh readme

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup, find_packages
+import re
+
+
+def parse_requirement(req):
+
+    if req.startswith("git+https"):
+        match = re.match(r"git\+https://github.com/([^/]+)/([^/]+).git", req)
+        if match:
+            package_name = match.group(2).replace("-", "_")
+            return f"{package_name} @ {req}"
+    return req
+
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
+
+requirements = [parse_requirement(req) for req in requirements]
 
 setup(
     name="radiointerferometry",
